a16ac994d9325ba63eaf1ec1ab3bcdc36bc52e74
lilia <lilianom59@outlook.fr>
2022-11-17 14:22:29 +0100

    exo51

---
+++ script.js
@@ -0,0 +1,15 @@
+let result = (5 + 6);
+
+let addition = (result + 2);
+
+let soustraction = (result - 1);
+
+alert(result * addition);
+
+alert(result && soustraction);
+
+let modulo = (5 % 2);
+
+document.write(modulo)
+
+
